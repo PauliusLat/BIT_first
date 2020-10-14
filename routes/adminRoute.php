57ff6d833337601ebc2005ac3dea0e79995542b7
PauliusLat <56875176+PauliusLat@users.noreply.github.com>
2020-10-14 23:46:25 +0300

    AdminRoute subMenu

--- routes/adminRoute.php
+++ routes/adminRoute.php
@@ -3,6 +3,8 @@
 return [
 	'admin' => 'AdminController@index',
 	'news' => 'NewsController@index',
+	'news@' => ['News List' => 'NewsController@index', 'Add New' => 'NewsController@create'],
+
 	'idejos' => 'IdeAdminController@adminIndex',
 
 	'galerija' => 'GalleryAdminController@adminIndex',
